f42c34dc73a3416099adb904604edba988e63625
artymmmm <arty1303@gmail.com>
2022-10-17 00:11:21 +0300

    Initial commit

---
+++ helper_functions.py
@@ -0,0 +1,134 @@
+import hashlib
+import random
+import math_functions as mfunc
+
+
+def get_hash_md5(message="Hello, world!"):
+    '''
+    Получение хеш-значение алгоритмом MD5
+    :param message: Сообщение
+    :type message: bytes
+    :return: Хеш-значение (основание 10)
+    :rtype: int
+    '''
+    hex_hash = hashlib.md5(message).hexdigest()
+    hash_message = int(hex_hash, 16)
+    return hash_message
+
+
+def generate_prime_nums(start, end, amount):
+    '''
+    Генерация простых чисел
+    :param start: Левая граница генерации
+    :type start: int
+    :param end: Правая граница генерации
+    :type end: int
+    :param amount: Количество чисел, которое требуется получить
+    :type amount: int
+    :return: Простые числа
+    :rtype: list
+    '''
+    generated_nums = []
+    prime_nums = []
+
+    while amount > 0:
+        num = random.randint(start, end)
+        if num not in generated_nums:
+            generated_nums.append(num)
+            if mfunc.prime_test_fermat(num, 100):
+                prime_nums.append(num)
+                amount -= 1
+
+    return prime_nums
+
+
+def generate_private_key(end):
+    '''
+    Генерация закрытого ключа
+    :param order: Порядок поля
+    :type order: int
+    :param lam: Значение функции Эйлера
+    :type eul_func: int
+    :return: Закрытый ключ
+    :rtype: int
+    '''
+    d = random.randint(2, end - 1)
+    if mfunc.gcd(d, end) != 1:
+        d = generate_private_key(end)
+    return d
+
+
+def get_key_shares(user_num, d, lam):
+    '''
+    Получение долей секретного ключа
+    Сумма долей секрета по модулю λ(n) равна секрету
+    :param user_num: Количество пользователей
+    :type user_num: int
+    :param d: Секретный ключ
+    :type d: int
+    :param lam: Значение функции Кармайкла
+    :type lam: int
+    :return: Доли секретного ключа
+    :rtype: list
+    '''
+    key_shares = []
+    sum = 0
+    for i in range(user_num - 1):
+        a = random.randint(1, lam - 1)
+        key_shares.append(a)
+        sum += a
+        sum = sum % lam
+    key_shares.append((d - sum) % lam)
+    return key_shares
+
+
+def get_partial_signs(key_shares, message, n):
+    '''
+    Получение подписей пользователей
+    :param key_shares: Доли ключей пользователей
+    :type key_shares: list
+    :param message: Подписываемое сообщение
+    :type message: int
+    :param n: Модуль
+    :type n: int
+    :return: Подписи пользователей
+    :rtype: list
+    '''
+    signs = []
+    for share in key_shares:
+        sign = mfunc.fast_pow_2(message, share, n)
+        signs.append(sign)
+    return signs
+
+
+def get_sign(partial_signs, n):
+    '''
+    Получение подписи из подписей пользователей
+    :param partial_signs: Подписи пользователей
+    :type partial_signs: list
+    :param n: Модуль
+    :type n: int
+    :return: Подпись сообщения
+    :rtype: int
+    '''
+    sign = 1
+    for partial_sign in partial_signs:
+        sign *= partial_sign
+        sign = sign % n
+    return sign
+
+
+def get_message(sign, e, n):
+    '''
+    Получение сообщения из подписи
+    :param sign: Подпись
+    :type sign: int
+    :param e: Открытый ключ
+    :type e: int
+    :param n: Модуль
+    :type n: int
+    :return: Сообщение
+    :rtype: int
+    '''
+    message = mfunc.fast_pow_2(sign, e, n)
+    return message
---
+++ math_functions.py
@@ -0,0 +1,252 @@
+import random
+
+
+def mod(a, m):
+    '''
+    Взятие числа по модулю
+    :param a: Число
+    :type a: int
+    :param m: Модуль
+    :type m: int
+    :return: Число по модулю
+    :rtype: int
+    '''
+    if m == 0:
+        raise ValueError("Деление на 0!")
+    if m == 1 or a == 0:
+        return 0
+
+    if m > 0:
+        if a > 0:
+            while a > 0:
+                a -= m
+            if a < 0:
+                a += m
+            return a
+        if a < 0:
+            while a < 0:
+                a += m
+            return a
+
+    else:
+        if a > 0:
+            while a > 0:
+                a += m
+            if a > 0:
+                a -= m
+            return a
+        if a < 0:
+            while a < 0:
+                a -= m
+            return a
+
+
+def gcd(a, b):
+    '''
+    НОД алгоритмом Евклида
+    :param a: Первое число
+    :type a: int
+    :param b: Второе число
+    :type b: int
+    :return: НОД
+    :rtype: int
+    '''
+    if a == b == 0:
+        raise ValueError("НОД(0, 0) не определен!")
+
+    a = abs(a)
+    b = abs(b)
+
+    if a < b:
+        a, b = b, a
+
+    if b == 0:
+        return a
+
+    while b != 0:
+        a, b = b, mod(a, b)
+    return int(a)
+
+
+def lcm(a, b):
+    '''
+    Нахождение НОК с помощью НОД
+    :param a: Первое число
+    :type a: int
+    :param b: Второе число
+    :type b: int
+    :return: НОК
+    :rtype: int
+    '''
+    if a == b == 0:
+        raise ValueError("НОК(0, 0) не определен!")
+    if a == 0 or b == 0:
+        return 0
+
+    a = abs(a)
+    b = abs(b)
+
+    res = int(a * (b / gcd(a, b)))
+    return res
+
+
+def fast_pow(a, b, m):
+    '''
+    Быстрое возведение в степень по модулю
+    :param a: Основание степени
+    :type a: int
+    :param b: Показатель степени
+    :type b: int
+    :param m: Модуль
+    :type m: int
+    :return: Результат возведения в степень
+    :rtype: int
+    '''
+    if b < 0:
+        raise ValueError(
+            "Показатель степени должен быть натуральным числом или 0!")
+    if b == 0:
+        return 1
+    if b == 1:
+        return mod(a, m)
+
+    a = mod(a, m)
+    if mod(b, 2) == 0:
+        res = fast_pow(a, b / 2, m)
+        product = mod(res * res, m)
+        return product
+    else:
+        res = fast_pow(a, b - 1, m)
+        product = mod(a * res, m)
+        return product
+
+
+def fast_pow_2(a, b, m):
+    '''
+    Быстрое возведение в степень по модулю с использование встроенной функции %
+    :param a: Основание степени
+    :type a: int
+    :param b: Показатель степени
+    :type b: int
+    :param m: Модуль
+    :type m: int
+    :return: Результат возведения в степень
+    :rtype: int
+    '''
+    if b < 0:
+        raise ValueError(
+            "Показатель степени должен быть натуральным числом или 0!")
+    if b == 0:
+        return 1
+    if b == 1:
+        return a % m
+
+    a = a % m
+    if b % 2 == 0:
+        res = fast_pow_2(a, b / 2, m)
+        product = (res * res) % m
+        return product
+    else:
+        res = fast_pow_2(a, b - 1, m)
+        product = (res * a) % m
+        return product
+
+
+def prime_test_fermat(p, precision):
+    '''
+    Тест Ферма на простоту числа
+    :param p: Проверяемое число
+    :type p: int
+    :param precision: Количество чисел, которые будут использованы при проверке.
+    Чем больше чисел, тем больше точность
+    :type precision: int
+    :return: Является ли число простым?
+    :rtype: bool
+    '''
+    if p < 0:
+        raise ValueError("Показатель степени должен быть натуральным числом!")
+    if p == 2 or p == 3:
+        return True
+    if mod(p, 2) == 0:
+        return False
+
+    for i in range(precision):
+        a = random.randint(2, p - 2)
+        if gcd(a, p) != 1:
+            continue
+        if fast_pow(a, p - 1, p) != 1:
+            return False
+    return True
+
+
+def carmichael_func(p, q):
+    '''
+    Функция Кармайкла
+    Для числа n = p * q находится как НОК(p - 1, q - 1)
+    :param p: Первое простое число
+    :type p: int
+    :param q: Второе простое число
+    :type q: int
+    :return: Значение функции Кармайкла
+    :rtype: int
+    '''
+    lam = lcm(p - 1, q - 1)
+    return lam
+
+def ext_euclidean_alg(a, b):
+    '''
+    Расширенный алгоритм Евклида
+    :param a: Первое число
+    :type a: int
+    :param b: Второе число
+    :type b: int
+    :return: НОД, корни уравнения ax + by = НОД(a, b)
+    :rtype: tuple
+    '''
+    a = abs(a)
+    b = abs(b)
+
+    if a < b:
+        a, b = b, a
+
+    x2 = 1
+    x1 = 0
+    y2 = 0
+    y1 = 1
+
+    while b != 0:
+        q = int(a / b)
+        r = a - q * b
+        x = x2 - q * x1
+        y = y2 - q * y1
+
+        a = b
+        b = r
+        x2 = x1
+        x1 = x
+        y2 = y1
+        y1 = y
+
+    d = a
+    x = x2
+    y = y2
+    return d, x, y
+
+
+def inverse_mod(a, m):
+    '''
+    Нахождение обратного по модулю через расширенный алгоритм Евклида
+    :param a: Число, к которому ищется обратное
+    :type a: int
+    :param m: Модуль
+    :type m: int
+    :return: Обратное
+    :rtype: int
+    '''
+    a = mod(a, m)
+    d, x, y = ext_euclidean_alg(m, a)
+
+    if d != 1:
+        raise ValueError("НОД(a, m) = 1, обратного значения не существует!")
+    else:
+        return mod(y, m)
