c0f587a83153331658dc8b0a459cea249aa0c47e
artymmmm <arty1303@gmail.com>
2022-10-18 21:33:29 +0300

    Added execution time

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import time
 import math_functions as mfunc
 import helper_functions as hfunc
 
 
-user_num = 10
+start_time = time.time()
+
+user_num = 3
 print(f"Количество пользователей - {user_num}\n")
 
-p, q = hfunc.generate_prime_nums(150, 100000, 2)
+p, q = hfunc.generate_prime_nums(150, 1000, 2)
 n = p * q
 lam = mfunc.carmichael_func(p, q)
 pq = lam / 2
@@ -14,7 +17,7 @@ d = mfunc.inverse_mod(e, lam)
 print(f"p = {p}, q = {q}, n = p * q = {n}, λ(n) = {lam}\n"
       f"e = {e}, d = {d}\n")
 
-message = 123423425
+message = 123456
 message_mod = mfunc.mod(message, n)
 print(f"Сообщение: {message}")
 print(f"Сообщение по модулю n: {message_mod}\n")
@@ -30,4 +33,6 @@ print(f"Подпись: {sign}\n")
 
 message_new = hfunc.get_message(sign, e, n)
 print(f"Исходное сообщение: {message_mod}\n"
-      f"Расшифрованное сообщение: {message_new}")
+      f"Расшифрованное сообщение: {message_new}\n")
+
+print("--- %s seconds ---" % (time.time() - start_time))
